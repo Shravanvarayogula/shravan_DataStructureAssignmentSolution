567457e6d749783b91469c05d76b98a1a0d1e860
Varayogula <shravan.varayogula@gmail.com>
2022-05-16 13:39:13 +0530

    Removing working branches

---
+++ Ques-2/src/com/greatlearning/dsaassignmenttwo/service/BSTNodeImplementation.java
@@ -0,0 +1,16 @@
+package com.greatlearning.dsaassignmenttwo.service;
+
+public class BSTNodeImplementation 
+{
+	int val;
+	public BSTNodeImplementation left,right;
+	public BSTNodeImplementation(int item)
+	{
+		val = item;
+		left = right = null;
+	}
+	public BSTNodeImplementation()
+	{
+	}
+}
+
